ff9c0fd1673ada8190809993a7042d650426d702
rmione <rmione33@gmail.com>
2019-11-16 23:03:43 -0500

    Figuring out serial stuff, basic skeleton laid down

---
+++ serial_practice.py
@@ -0,0 +1,42 @@
+import serial
+import struct
+
+# todo: Here we have the baud rate, defined in the lecture slides/serial guide.
+#   No idea what serial port we're going to be using, so for now it will be set to COM2.
+#   We definitely need to have parity bits... I don't know how even to use a parity bit... is the error detection done
+#   automatically, or...?
+
+
+"""
+I imagine that this will be best done by a function, or we could hard code it each time with a unique version, depending 
+on which pacing mode we use, etc., since we will be using more or less parametes depending on a few things.
+
+Or we can have our function, either taking them all and defaulting the optional parameters to None, OR
+we could have kwargs which sort of accomplish the same goal anyways.
+"""
+
+
+def to_bytes(mode, low, up, AAmp, VAmp, APW, VPW, ASense, VSense, ARP, VRP, MaxSense, PVARP, FAVD, user):
+    """
+    Going to need some sort of order here.
+    Judging by the params we have laid out, something like this: 
+        mode:lowerratelim:upperratelim:... and so on, and so forth
+    """
+
+
+    # todo:
+    #  for now we are going to assume longs. No idea as to what behaviour this entails but could be a catch all!!!!
+    #  keep in mind these are signed longs, so i think it will work as a catch-all
+
+    # Something like this seems to be the proper thing to do. We'll have to see
+
+    # We can return this array of bytes for sending over serial
+    return bytearray(struct.pack('lllddllddllllll', mode, low, up, AAmp, VAmp, APW, VPW, ASense, VSense, ARP, VRP, MaxSense, PVARP, FAVD, user)) # todo: honestly don't know if this is fine or not. We'll have to see.
+
+
+
+baud_rate = 115200
+# You can define Serial objects with unique parameters, so we can have different parities, bytesize etc. will be handy!!
+board = serial.Serial(port='COM6', baudrate=baud_rate, parity=serial.PARITY_NONE, bytesize=8)
+board.write(to_bytes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)) # Writing to serial seems to be fairly simple
+
